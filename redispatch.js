b0de7a7dd9cc40b2da877e8a4aa98b469b23e395
Kevin Beaty <kevin@simplectic.com>
2014-11-18 17:02:13 -0600

    Allow binding this. Add unregister

--- redispatch.js
+++ redispatch.js
@@ -3,20 +3,39 @@ var undef;
 
 module.exports = redispatch;
 
-function redispatch(){
+function redispatch(ctx){
   var fns = [],
-      d = dispatch(fns);
-  d.register = function(fn){
-    fns.push(fn);
-  };
+      d = dispatch(fns, ctx);
+
+  d.register = register(fns);
+  d.unregister = unregister(fns);
+
   return d;
 }
 
-function dispatch(fns){
+function register(fns){
+  return function(fn){
+    fns.push(fn);
+  };
+}
+
+function unregister(fns){
+  return function(fn){
+    var idx = fns.indexOf(fn);
+    if(idx > -1){
+      fns.splice(idx, 1);
+    }
+  };
+}
+
+function dispatch(fns, ctx){
   return function(){
-    var args = arguments, i = fns.length, result;
+    var args = arguments,
+        self = ctx !== undef ? ctx : this,
+        i = fns.length,
+        result;
     for(; i-- ;){
-      result = fns[i].apply(this, args);
+      result = fns[i].apply(self, args);
       if(result !== undef){
         return result;
       }
--- test/tests.js
+++ test/tests.js
@@ -12,16 +12,63 @@ test('dispatch', function(t){
 
   t.equals(null, empty(null));
   t.deepEquals(empty([1,2,3]), null);
-  
 
-  empty.register(function(arr){
-    if(Array.isArray(arr)){
-      return [];
-    }
-  });
+  empty.register(emptyArray);
 
   t.equals(null, empty(null));
   t.deepEquals(empty([1,2,3]), []);
-  
+
+  empty.unregister(emptyArray);
+
+  t.equals(null, empty(null));
+  t.deepEquals(empty([1,2,3]), null);
+
   t.end();
+
+  function emptyArray(arr){
+    if(Array.isArray(arr)){
+      return [];
+    }
+  }
+});
+
+test('context', function(t){
+  var dog = {name: 'fido'},
+      spot = {name: 'spot'},
+      speak = dispatch();
+  dog.say = dispatch(dog);
+
+  speak.register(ruff);
+  dog.say.register(ruff);
+
+  t.equals('ruff fido', speak.call(dog));
+  t.equals('ruff spot', speak.call(spot));
+  t.equals('ruff fido', dog.say());
+
+  speak.register(bark);
+  dog.say.register(bark);
+
+  t.equals('bark fido', speak.call(dog));
+  t.equals('bark spot', speak.call(spot));
+  t.equals('bark fido', dog.say());
+
+  speak.unregister(bark);
+  dog.say.unregister(bark);
+
+  t.equals('ruff fido', speak.call(dog));
+  t.equals('ruff spot', speak.call(spot));
+  t.equals('ruff fido', dog.say());
+
+  t.end();
+
+  function ruff(){
+    /*jshint validthis:true */
+    return 'ruff '+this.name;
+  }
+
+  function bark(){
+    /*jshint validthis:true */
+    return 'bark '+this.name;
+  }
+
 });
